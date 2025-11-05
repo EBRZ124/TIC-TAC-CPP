2ed9e5983cf40ada6f021b8e9504078d42b38aed
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-11-05 22:26:38 +0200

    Update tic-tac-toe.cpp
    X and Y coordinates added for where the Xs and Os should be "drawn" for their corresponding locations

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -12,24 +12,30 @@ int main()
     InitWindow(screenWidth, screenHeight, "raylib [core] example - input actions");
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
-
-    Texture2D background = LoadTexture("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/background.png");
-    Texture2D playingGrid = LoadTexture("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/playingGrid.png");
-    Button playButton{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/play-button.png",{524, 0}, 0.20};
-    Button exitButton{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/exit-button.png", {592, 412}, 0.10};
+    Texture2D cross = LoadTexture("/Graphics/cross.png");
+    Texture2D circle = LoadTexture("/Graphics/circle.png");
+    Texture2D background = LoadTexture("/Graphics/background.png");
+    Texture2D playingGrid = LoadTexture("/Graphics/playingGrid.png");
+    Button playButton{"/Graphics/play-button.png",{524, 0}, 0.20};
+    Button exitButton{"/Graphics/exit-button.png", {592, 412}, 0.10};
     bool exit = false;
+    
+    srand(time(NULL));
+
+    int currentEnemyAttack = -1;
+    int currentPlayerAttack = -1;
 
     // Main game loop
-    while(!WindowShouldClose() && exit == false){
+    while (!WindowShouldClose() && !exit) {
         Vector2 mousePosition = GetMousePosition();
         bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
 
-        if(playButton.isPressed(mousePosition, mousePressed)){
-            srand(time(NULL));
-            // int randomnumber = rand() % 2;
-            int randomnumber = 0;
+        if (playButton.isPressed(mousePosition, mousePressed)) {
+            int enemy_attack = rand() % 9;
+            currentEnemyAttack = enemy_attack;
         }
-        if(exitButton.isPressed(mousePosition, mousePressed)){
+
+        if (exitButton.isPressed(mousePosition, mousePressed)) {
             exit = true;
         }
 
@@ -39,6 +45,19 @@ int main()
         DrawTexture(playingGrid, 15, 15, WHITE);
         exitButton.Draw();
         playButton.Draw();
+
+        // Draw based on stored game state (not just when clicked)
+        if (currentEnemyAttack >= 0) {
+            int xPos[9] = {50, 200, 350, 50, 200, 350, 50, 200, 350};
+            int yPos[9] = {30, 30, 30, 190, 190, 190, 340, 340, 340};
+            
+            int drawX = 350;
+            int drawY = 30;
+            Vector2 pos = { static_cast<float>(drawX), static_cast<float>(drawY) };
+            float scale = 0.25f;
+            DrawTextureEx(circle, pos, 0.0f, scale, WHITE);
+        }
+
         EndDrawing();
     }
 
