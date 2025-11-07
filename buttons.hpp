8cc708121919145d0ab38454de54f78211cf0e52
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-11-07 18:56:13 +0200

    Update buttons.hpp

--- buttons.hpp
+++ buttons.hpp
@@ -1,14 +1,22 @@
 #pragma once
-#include"raylib.h"
+#include "raylib.h"
 
+class Button {
+public:
+    Button();
+    Button(const char* imagePath, Vector2 imagePosition, float scale);
+    ~Button();
 
-class Button{
-    public:
-        Button(const char* imagePath, Vector2 imagePosition, float scale);
-        ~Button();
-        void Draw();
-        bool isPressed(Vector2 mousePos, bool mousePressed);
-    private:
-        Texture2D texture;
-        Vector2 position;
+    void init(const char* imagePath, Vector2 imagePosition, float scale);
+    void Draw();
+    bool isPressed(Vector2 mousePos, bool mousePressed);
+
+    // Optional helpers
+    Vector2 getPosition() const { return position; }
+    float getWidth() const { return static_cast<float>(texture.width); }
+    float getHeight() const { return static_cast<float>(texture.height); }
+
+private:
+    Texture2D texture;
+    Vector2 position;
 };
