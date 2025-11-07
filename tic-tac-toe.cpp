d68fd4cac79552ad1c01841602d967eeb06007a1
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-11-07 18:55:42 +0200

    Update tic-tac-toe.cpp
    Optimised button algorithm (no more 9 if statements, but one within a for loop)

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -28,6 +28,17 @@ int main()
     Button exitButton{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/exit-button.png", {592, 412}, 0.10};
     
     // Placemenet buttons for the player/user
+    Button player_placement_buttons[9];
+    player_placement_buttons[0] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {25, 15}, 0.3);  
+    player_placement_buttons[1] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {180, 15}, 0.3);  
+    player_placement_buttons[2] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {340, 15}, 0.3);  
+    player_placement_buttons[3] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {25, 15}, 0.3);  
+    player_placement_buttons[4] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {180, 175}, 0.3);  
+    player_placement_buttons[5] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {340, 175}, 0.3);  
+    player_placement_buttons[6] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {25, 175}, 0.3);  
+    player_placement_buttons[7] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {180, 330}, 0.3);  
+    player_placement_buttons[8] = Button("/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {340, 330}, 0.3);    
+    /*
     Button placementButton1{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {25, 15}, 0.30};
     Button placementButton2{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {180, 15}, 0.30};
     Button placementButton3{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {340, 15}, 0.30};
@@ -37,6 +48,7 @@ int main()
     Button placementButton7{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {25, 330}, 0.30};
     Button placementButton8{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {180, 330}, 0.30};
     Button placementButton9{"/Users/evaldsberzins/raylib/projects/tic-tac-toe/Graphics/button-test.png", {340, 330}, 0.30};
+    */
     bool exit = false;
     
     srand(time(NULL));
@@ -101,15 +113,14 @@ int main()
         if (exitButton.isPressed(mousePosition, mousePressed)) {
             exit = true;
         }
-            
-            // PLayers attacks
-            if(placementButton1.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[0]){
-                    std::cout<<"Field Taken";
+        for (int i = 0; i < 9; i++) {
+            if (player_placement_buttons[i].isPressed(mousePosition, mousePressed)) {
+                if (TakenFields[i]) {
+                    std::cout << "Field Taken" << std::endl;
                 } else {
-                    playerAttacks[0] = true;
-                    TakenFields[0] = true;
-                    fields_taken_value[0] = 1;
+                    playerAttacks[i] = true;
+                    TakenFields[i] = true;
+                    fields_taken_value[i] = 1;
 
                     enemyThinking = true;
                     enemyMoveTime = GetTime() + 1.5;
@@ -118,194 +129,23 @@ int main()
                     showEnemyMoveIndicator = true;
 
                     int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
+                    while (TakenFields[randomField]) {
                         randomField = rand() % 9;
                     }
-                    pendingEnemyAttack = randomField;
-                    
-                }
-            }
-            if(placementButton2.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[1]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[1] = true;
-                    TakenFields[1] = true;
-                    fields_taken_value[1] = 1;
 
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    showPlayerMoveIndicator = false;
-                    showEnemyMoveIndicator = true;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
                     pendingEnemyAttack = randomField;
                 }
             }
-            if(placementButton3.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[2]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[2] = true;
-                    TakenFields[2] = true;
-                    fields_taken_value[2] = 1;
-
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    showPlayerMoveIndicator = false;
-                    showEnemyMoveIndicator = true;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
-                    pendingEnemyAttack = randomField;
-                }
-            }
-            if(placementButton4.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[3]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[3] = true;
-                    TakenFields[3] = true;
-                    fields_taken_value[3] = 1;
-
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
-                    pendingEnemyAttack = randomField;
-                }
-            }
-            if(placementButton5.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[4]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[4] = true;
-                    TakenFields[4] = true;
-                    fields_taken_value[4] = 1;
-
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    showPlayerMoveIndicator = false;
-                    showEnemyMoveIndicator = true;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
-                    pendingEnemyAttack = randomField;
-                }
-            }
-            if(placementButton6.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[5]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[5] = true;
-                    TakenFields[5] = true;
-                    fields_taken_value[5] = 1;
-
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    showPlayerMoveIndicator = false;
-                    showEnemyMoveIndicator = true;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
-                    pendingEnemyAttack = randomField;
-                }
-            }
-            if(placementButton7.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[6]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[6] = true;
-                    TakenFields[6] = true;
-                    fields_taken_value[6] = 1;
-
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    showPlayerMoveIndicator = false;
-                    showEnemyMoveIndicator = true;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
-                    pendingEnemyAttack = randomField;
-                }
-            }
-            if(placementButton8.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[7]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[7] = true;
-                    TakenFields[7] = true;
-                    fields_taken_value[7] = 1;
-
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    showPlayerMoveIndicator = false;
-                    showEnemyMoveIndicator = true;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
-                    pendingEnemyAttack = randomField;
-                }
-            }
-            if(placementButton9.isPressed(mousePosition, mousePressed)){
-                if(TakenFields[8]){
-                    std::cout<<"Field Taken";
-                } else {
-                    playerAttacks[8] = true;
-                    TakenFields[8] = true;
-                    fields_taken_value[8] = 1;
-
-                    enemyThinking = true;
-                    enemyMoveTime = GetTime() + 1.5;
-
-                    showPlayerMoveIndicator = false;
-                    showEnemyMoveIndicator = true;
-
-                    int randomField = rand() % 9;
-                    while(TakenFields[randomField]){
-                        randomField = rand() % 9;
-                    }
-                    pendingEnemyAttack = randomField;
-                }
-            }
-            
+        }
         BeginDrawing();
         ClearBackground(BLACK);
+        for(int fieldButton=0; fieldButton<9; fieldButton++){
+            player_placement_buttons[fieldButton].Draw();
+        }
         DrawTexture(background, 0, 0, WHITE);
         DrawTexture(playingGrid, 15, 15, WHITE);
         exitButton.Draw();
         playButton.Draw();
-        placementButton1.Draw();
-        placementButton2.Draw();
-        placementButton3.Draw();
-        placementButton4.Draw();
-        placementButton5.Draw();
-        placementButton6.Draw();
-        placementButton7.Draw();
-        placementButton8.Draw();
-        placementButton9.Draw();
 
         if(showEnemyMoveIndicator){
             DrawTextureEx(enemy_move_indicator, move_indicator_pos, 0.0f, 0.15f, WHITE);
@@ -352,7 +192,6 @@ int main()
                 }
             }   
         }
-
         EndDrawing();
     }
 
